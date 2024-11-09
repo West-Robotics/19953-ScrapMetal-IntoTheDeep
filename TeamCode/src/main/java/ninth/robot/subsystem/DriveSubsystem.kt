461c499cda9580ff814f51758c1e11457edf7d4d
Maria <maria@debian.com>
2024-11-08 23:31:51 -0900

    feat: create drive, intake, and lift subsystems

---
+++ TeamCode/src/main/java/ninth/robot/subsystem/DriveSubsystem.kt
@@ -0,0 +1,8 @@
+package ninth.robot.subsystem
+
+import com.qualcomm.robotcore.hardware.DcMotor
+import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion
+
+class DriveSubsystem {
+    private val frontLeft: `val` = BlocksOpModeCompanion.hardwareMap.get(DcMotor::DcMotor::)
+}
