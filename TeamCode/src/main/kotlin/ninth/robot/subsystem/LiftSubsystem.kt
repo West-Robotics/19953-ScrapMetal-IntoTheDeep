f02fd77418514824313585b1e667a6d5bdf8237c
riddle-wrapped <mariakingry@proton.me>
2024-11-10 22:24:26 -0900

    feat: add lift presets, set up edge detectors

--- TeamCode/src/main/kotlin/ninth/robot/subsystem/LiftSubsystem.kt
+++ TeamCode/src/main/kotlin/ninth/robot/subsystem/LiftSubsystem.kt
@@ -2,23 +2,58 @@ package ninth.robot.subsystem
 
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.Servo
 import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap
+import kotlin.math.PI
 
-class LiftSubsystem {
-    class DriveSubsystem {
-        private val liftOne = hardwareMap.get(DcMotor::class.java, "liftOne")
-        private val liftTwo = hardwareMap.get(DcMotor::class.java, "liftTwo")
+class LiftSubsystem(hardwareMap: HardwareMap) {
+    // remember to plug the encoder into the liftOne port!
+    private val liftOne = hardwareMap.get(DcMotor::class.java, "liftOne")
+    private val liftTwo = hardwareMap.get(DcMotor::class.java, "liftTwo")
 
-        init {
-            liftOne.setDirection(DcMotorSimple.Direction.FORWARD)
-            liftTwo.setDirection(DcMotorSimple.Direction.REVERSE)
+    init {
+        liftOne.setDirection(DcMotorSimple.Direction.FORWARD)
+        liftTwo.setDirection(DcMotorSimple.Direction.REVERSE)
 
-            liftOne.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
-            liftTwo.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+        liftOne.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+        liftTwo.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
 
-            liftOne.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
-            liftTwo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+        liftOne.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+        liftTwo.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+    }
+
+    fun resetEncoder() {
+        liftOne.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER)
+    }
+
+    val driveCPR = 8192
+    val wheelCircumference: Double = 10.4 * PI
+
+    fun centimetersToTicks(cm: Double): Int {
+        return (cm * driveCPR/wheelCircumference).toInt()
+    }
+
+    fun ticksToCentimeters(ticks: Int): Double {
+        return (ticks * wheelCircumference / driveCPR)
+    }
+
+    fun extensionSpeed(speed: Double) {
+        liftOne.setPower(speed)
+        liftTwo.setPower(speed)
+    }
+
+    fun getHeight(): Double {
+        val liftDisplacement = ticksToCentimeters(liftOne.getCurrentPosition())
+        return liftDisplacement
+    }
+
+    fun runToPreset(preset: Double, currentHeight: Double, speed: Double): Double {
+        val speedForLoop = when {
+            currentHeight < preset -> speed
+            currentHeight > preset -> -speed
+            else -> 0.0
         }
+        return speedForLoop
     }
 }
