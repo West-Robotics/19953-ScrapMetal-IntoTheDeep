7376c9430e224185acec4b979cda00a3626dcdf5
consla <conslaaa@proton.me>
2024-11-17 16:14:33 -0900

    feat(Util): add MP illegal argument exceptions

--- Util/src/main/kotlin/com/scrapmetal/util/control/Utils.kt
+++ Util/src/main/kotlin/com/scrapmetal/util/control/Utils.kt
@@ -39,20 +39,26 @@ fun constrainEffort(
 // WARNING: untested
 // TODO: test that it works
 // TODO: measure performance on chub
-//  run a bunch and time
+//  run a bunch and time it
+// NOTE: could potentially move to its own class to avoid redundant calculations
 /**
  * Returns position, velocity, and acceleration information for a trapezoidal motion profile
  * given movement [constraints] and a time [t]
  */
 fun motionProfile(constraints: MPConstraints, t: Double): MPState {
     with (constraints) {
+        require(accel > 0.0) { "Acceleration must be > 0" }
+        require(vLimit >= 0.0) { "Acceleration must be >= 0" } // >= is not a typo
+        require(decel > 0.0) { "Deceleration must be > 0" }
         // signed
         val s = end - start
         // unsigned
         val vMax = min(vLimit, accel * sqrt( abs(s) / (0.5 * accel * (1 + accel/decel))))
         val tA = vMax / accel
         val tD = tA * accel / decel
-        val tV = (abs(s) - 0.5 * accel * tA.pow(2) - 0.5 * decel * tD.pow(2)) / abs(vMax)
+        val tV = if (vMax != 0.0) {
+            (abs(s) - 0.5 * accel * tA.pow(2) - 0.5 * decel * tD.pow(2)) / abs(vMax)
+        } else 0.0
 
         // all signed
         val a = accel * sign(s)
@@ -60,13 +66,13 @@ fun motionProfile(constraints: MPConstraints, t: Double): MPState {
         val d = decel * sign(s)
 
         return MPState(
-            start +
+            s = start +
                    0.5 * a * min(max(t, 0.0), tA).pow(2) +
                    v * min(max(t-tA, 0.0), tV) +
                    v * min(max(t-tA-tV, 0.0), tD) -
                        0.5 * d * min(max(t-tA-tV, 0.0), tD).pow(2),
-            listOf(a * t, v, v - d * (t-tA-tV)).minBy { abs(it) },
-            when (t) {
+            v = listOf(a * t, v, v - d * (t-tA-tV)).minBy { abs(it) },
+            a = when (t) {
                 in 0.0..tA -> a
                 in tA..tA+tV -> 0.0
                 in tA+tV..tA+tV+tD -> -d
