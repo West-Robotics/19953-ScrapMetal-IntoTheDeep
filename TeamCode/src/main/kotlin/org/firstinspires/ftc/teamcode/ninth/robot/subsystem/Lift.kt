62656999a7ad6f8dd8899a3e8a440049a061fc0c
riddle-wrapped <mariakingry@proton.me>
2024-11-23 12:38:18 -0900

    refactor: switch to using sm motor and quadrature wrappers

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/ninth/robot/subsystem/Lift.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/ninth/robot/subsystem/Lift.kt
@@ -3,42 +3,33 @@ package org.firstinspires.ftc.teamcode.ninth.robot.subsystem
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
+import com.scrapmetal.util.hardware.SMMotor
+import com.scrapmetal.util.hardware.SMQuadrature
 import kotlin.math.PI
 
 class Lift(hardwareMap: HardwareMap) {
-    private val left = hardwareMap.dcMotor.get("leftLift")
-    private val right = hardwareMap.dcMotor.get("rightLift")
-    // TODO: rename encoder to drive motor
-    private val encoder = hardwareMap.dcMotor.get("frontRight")
+    val cpr = 8192.0
+    val spoolCircumference = 0.7874016 * PI
 
-    init {
-        left.setDirection(DcMotorSimple.Direction.FORWARD)
-        right.setDirection(DcMotorSimple.Direction.REVERSE)
+    private val left = SMMotor(hardwareMap, "leftLift", DcMotorSimple.Direction.FORWARD, DcMotor.ZeroPowerBehavior.BRAKE)
+    private val right = SMMotor(hardwareMap, "rightLift", DcMotorSimple.Direction.REVERSE, DcMotor.ZeroPowerBehavior.BRAKE)
 
-        left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
-        right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
-
-        left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
-        right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
-    }
+    private val encoder = SMQuadrature(hardwareMap, "frontRight", spoolCircumference/cpr, 1.0/cpr, DcMotorSimple.Direction.REVERSE)
 
     fun resetEncoder() {
-        encoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER)
+        encoder.reset()
     }
-
-    val cpr = 8192
-    val spoolCircumference = 0.7874016 * PI
 
     fun inchesToTicks(inches: Double) = (inches * cpr/spoolCircumference).toInt()
 
     fun ticksToInches(ticks: Int) = ticks * spoolCircumference / cpr
 
     fun setPower(power: Double) {
-        left.setPower(power)
-        right.setPower(power)
+        left.effort = power
+        right.effort = power
     }
 
-    fun getHeight() = ticksToInches(encoder.getCurrentPosition())
+    fun getHeight() = encoder.dist
 
     fun runToPreset(preset: Double, currentHeight: Double, power: Double): Double {
         val speedForLoop = when {
