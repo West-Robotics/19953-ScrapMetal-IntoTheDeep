ba46849fa9f7534e0fb0d20632da605335607463
riddle-wrapped <mariakingry@proton.me>
2024-11-27 23:43:19 -0900

    feat(Util): migrate servo and crservo wrappers from Quackarama

--- Util/src/main/kotlin/com/scrapmetal/util/hardware/SMServo.kt
+++ Util/src/main/kotlin/com/scrapmetal/util/hardware/SMServo.kt
@@ -1,4 +1,74 @@
 package com.scrapmetal.util.hardware
 
-class SMServo {
+import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.PwmControl
+import com.qualcomm.robotcore.hardware.Servo
+import com.qualcomm.robotcore.hardware.ServoImpl
+import com.qualcomm.robotcore.hardware.ServoImplEx
+import kotlin.math.abs
+
+/**
+ * Servo wrapper with cached writes, built in servo pwm ranges, and fewer functions
+ */
+
+/**
+ * @param pwm servo model, used to determine pwm range
+ * @param pwm servo model, used to determine pwm range
+ * @param usFrame framing rate in microseconds
+ */
+
+// TODO: add direction and initial pos as part of constructor so they are explicitly stated
+
+class SMServo(
+    hardwareMap: HardwareMap,
+    name: String,
+    pwm: ModelPWM,
+    private var thresh: Double = 0.002,
+    usFrame: Double = 5000.0,
+) {
+    /**
+     * PWM ranges for various servo models
+     *
+     * Axons are limited from 510-2490 to prevent accidental wraparound (may be adjusted) in the future
+     */
+    enum class ModelPWM(val min: Double, val max: Double) {
+        AXON_MAX(510.0, 2490.0), AXON_MINI(510.0, 2490.0), AXON_MICRO(510.0, 2490.0),
+        GOBILDA_TORQUE(500.0, 2500.0), GOBILDA_SPEED(500.0, 2500.0), GOBILDA_SUPER(500.0, 2500.0),
+        GENERIC(500.0, 2500.0),
+    }
+
+    private val servo = hardwareMap.get(ServoImplEx::class.java, name)
+    private var lastPosition = 0.0
+
+    init {
+        servo.pwmRange = PwmControl.PwmRange(pwm.min, pwm.max, usFrame)
+    }
+
+    fun direction(direction: Servo.Direction) {
+        servo.direction = direction
+    }
+
+    fun thresh(thresh: Double) {
+        this.thresh = thresh
+    }
+
+    fun position(position: Double) {
+        if (abs(position - lastPosition) > thresh) {
+            servo.setPosition(position)
+            lastPosition = position
+        }
+    }
+
+    /**
+     * @param convertToPosition convert position argument to servo's 0-1 range
+     */
+    fun position(position: Double, convertToPosition: (Double) -> Double) {
+        val convertedPosition = convertToPosition(position)
+        if (abs(convertedPosition - lastPosition) > thresh) {
+            servo.setPosition(convertedPosition)
+            lastPosition = convertedPosition
+        }
+    }
+
+    fun getCommandPosition() = servo.position
 }
