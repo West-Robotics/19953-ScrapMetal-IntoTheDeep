fc7ca1afbff095ce9b3bb4f5b95322fc2b77e70c
consla <conslaaa@proton.me>
2024-11-27 18:34:31 -0900

    test(Util): add Movement builder test

--- Util/src/test/kotlin/PathingTest.kt
+++ Util/src/test/kotlin/PathingTest.kt
@@ -1,31 +1,37 @@
 import com.scrapmetal.util.control.Vector2d
+import com.scrapmetal.util.control.pathing.Constant
+import com.scrapmetal.util.control.pathing.Linear
 import com.scrapmetal.util.control.pathing.Spline
-import com.scrapmetal.util.control.pathing.closestPoint
-import com.scrapmetal.util.control.pathing.closestPointBruteforce
+import com.scrapmetal.util.control.pathing.SplinePoint
+import com.scrapmetal.util.control.pathing.SubMovement
+import com.scrapmetal.util.control.pathing.Tangent
+import com.scrapmetal.util.control.pathing.to
+import com.scrapmetal.util.control.pathing.withEffort
+import com.scrapmetal.util.control.pathing.withHeading
 import kotlin.random.Random
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.time.measureTimedValue
 
 class PathingTest {
-    val spline = Spline(
-        start = Vector2d(0.0, 0.0),
-        v0 = Vector2d(26.0, 10.0),
-        end = Vector2d(24.0, 2.0),
-        v1 = Vector2d(0.0, 40.0),
-    )
-
     @Test
     fun testClosestT() {
+        val spline = Spline(
+            start = Vector2d(0.0, 0.0),
+            startTangent = Vector2d(26.0, 10.0),
+            end = Vector2d(24.0, 2.0),
+            endTangent = Vector2d(0.0, 40.0),
+        )
         repeat (1000) {
             val pos = Vector2d(
                 Random.nextDouble(-20.0, 50.0),
                 Random.nextDouble(-20.0, 20.0)
             )
             println("pos = $pos")
-            val (efficientClosestPoint, efficientTime) = measureTimedValue { closestPoint(pos, spline) }
+            val (efficientClosestPoint, efficientTime) = measureTimedValue { spline.closestPoint(pos) }
             println("efficient = $efficientClosestPoint")
             println("efficient time = $efficientTime")
-            val (bruteForceClosestPoint, bruteForceTime) = measureTimedValue { closestPointBruteforce(pos, spline) }
+            val (bruteForceClosestPoint, bruteForceTime) = measureTimedValue { spline.closestPointBruteforce(pos) }
             println("brute force = $bruteForceClosestPoint")
             println("brute force time = $bruteForceTime")
             println("")
@@ -34,4 +40,46 @@ class PathingTest {
         }
     }
 
+    @Test
+    fun testMovementBuilder() {
+        val movement = SplinePoint(0.0, 0.0, 26.0, 10.0) to
+                SplinePoint(24.0, 2.0, 0.0, 40.0) withHeading Constant(10.0) to
+                SplinePoint(1.0, 2.0, 3.0, 4.0) to
+                SplinePoint(5.0, 6.0, 7.0, 8.0) withEffort 0.2 to
+                SplinePoint(9.0, 10.0, 11.0, 2.0) withHeading Linear(10.0, 180.0) withEffort 0.5
+
+        assertEquals(movement.submovements.size, 4)
+        assertEquals(
+            movement.submovements[0],
+            SubMovement(
+                Spline(Vector2d(0.0, 0.0), Vector2d(26.0, 10.0), Vector2d(24.0, 2.0), Vector2d(0.0, 40.0)),
+                Constant(10.0),
+                1.0,
+            )
+        )
+        assertEquals(
+            movement.submovements[1],
+            SubMovement(
+                Spline(Vector2d(24.0, 2.0), Vector2d(0.0, 40.0), Vector2d(1.0, 2.0), Vector2d(3.0, 4.0)),
+                Tangent(Spline(Vector2d(24.0, 2.0), Vector2d(0.0, 40.0), Vector2d(1.0, 2.0), Vector2d(3.0, 4.0))),
+                1.0,
+            )
+        )
+        assertEquals(
+            movement.submovements[2],
+            SubMovement(
+                Spline(Vector2d(1.0, 2.0), Vector2d(3.0, 4.0), Vector2d(5.0, 6.0), Vector2d(7.0, 8.0)),
+                Tangent(Spline(Vector2d(1.0, 2.0), Vector2d(3.0, 4.0), Vector2d(5.0, 6.0), Vector2d(7.0, 8.0))),
+                0.2,
+            )
+        )
+        assertEquals(
+            movement.submovements[3],
+            SubMovement(
+                Spline(Vector2d(5.0, 6.0), Vector2d(7.0, 8.0), Vector2d(9.0, 10.0), Vector2d(11.0, 2.0)),
+                Linear(10.0, 180.0),
+                0.5,
+            )
+        )
+    }
 }
