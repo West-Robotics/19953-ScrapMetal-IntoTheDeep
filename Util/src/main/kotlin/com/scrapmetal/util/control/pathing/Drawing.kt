40fe56f4b66a5903e7cd03218c2c6598b78afbf1
consla <conslaaa@proton.me>
2024-11-28 20:15:32 -0900

    style(Util): rename constants to screaming snake case

--- Util/src/main/kotlin/com/scrapmetal/util/control/pathing/Drawing.kt
+++ Util/src/main/kotlin/com/scrapmetal/util/control/pathing/Drawing.kt
@@ -4,55 +4,55 @@ import com.acmerobotics.dashboard.canvas.Canvas
 import com.scrapmetal.util.control.Pose2d
 import com.scrapmetal.util.control.Vector2d
 
-const val robotColor = "#00ff00"
-const val pathColor = "#0000ff"
-const val robotRadius = 7.5
-const val pointCount = 256
-const val headingCount = 16
-const val trailCount = 512
-const val trailRadius = 0.125
+const val ROBOT_COLOR = "#00ff00"
+const val PATH_COLOR = "#0000ff"
+const val ROBOT_RADIUS = 7.5
+const val POINT_COUNT = 256
+const val HEADING_COUNT = 16
+const val TRAIL_COUNT = 512
+const val TRAIL_RADIUS = 0.125
 // TODO: does this need a flush between opmodes?
 // TODO: use a better data structure
 val trail = ArrayDeque<Vector2d>(512)
 
 fun Canvas.drawRobot(pose: Pose2d): Canvas {
-    val headingTip = pose.position + pose.heading*Vector2d(robotRadius, 0.0)
-    this.setStroke(robotColor)
-        .strokeCircle(pose.position.x, pose.position.y, robotRadius)
+    val headingTip = pose.position + pose.heading*Vector2d(ROBOT_RADIUS, 0.0)
+    this.setStroke(ROBOT_COLOR)
+        .strokeCircle(pose.position.x, pose.position.y, ROBOT_RADIUS)
         .strokeLine(pose.position.x, pose.position.y, headingTip.x, headingTip.y)
     return this
 }
 
 fun Canvas.drawPath(spline: Spline): Canvas {
-    val points = Array(pointCount) { spline(it/pointCount.toDouble()) }
-    val xPoints = DoubleArray(pointCount) { points[it].x }
-    val yPoints = DoubleArray(pointCount) { points[it].y }
-    this.setStroke(pathColor)
+    val points = Array(POINT_COUNT) { spline(it/POINT_COUNT.toDouble()) }
+    val xPoints = DoubleArray(POINT_COUNT) { points[it].x }
+    val yPoints = DoubleArray(POINT_COUNT) { points[it].y }
+    this.setStroke(PATH_COLOR)
         .strokePolyline(xPoints, yPoints)
     return this
 }
 
 fun Canvas.drawPathWithHeading(spline: Spline, heading: HeadingInterpolation): Canvas {
-    val points = Array(pointCount) { spline(it/pointCount.toDouble()) }
-    val xPoints = DoubleArray(pointCount) { points[it].x }
-    val yPoints = DoubleArray(pointCount) { points[it].y }
-    val headingTipPoints = Array(headingCount) {
+    val points = Array(POINT_COUNT) { spline(it/POINT_COUNT.toDouble()) }
+    val xPoints = DoubleArray(POINT_COUNT) { points[it].x }
+    val yPoints = DoubleArray(POINT_COUNT) { points[it].y }
+    val headingTipPoints = Array(HEADING_COUNT) {
         Pair(
-            points[it*pointCount/headingCount],
-            points[it*pointCount/headingCount] +
-                heading(it/headingCount.toDouble())*Vector2d(robotRadius/4, 0.0)
+            points[it*POINT_COUNT/HEADING_COUNT],
+            points[it*POINT_COUNT/HEADING_COUNT] +
+                heading(it/HEADING_COUNT.toDouble())*Vector2d(ROBOT_RADIUS/4, 0.0)
         )
     }
-    this.setStroke(pathColor)
+    this.setStroke(PATH_COLOR)
         .strokePolyline(xPoints, yPoints)
     headingTipPoints.forEach { this.strokeLine(it.first.x, it.first.y, it.second.x, it.second.y) }
     return this
 }
 
 fun Canvas.drawTrail(position: Vector2d): Canvas {
-    if (trail.size == trailCount) trail.removeFirst()
+    if (trail.size == TRAIL_COUNT) trail.removeFirst()
     trail.add(position)
-    this.setStroke(robotColor)
-    trail.forEach { this.fillCircle(it.x, it.y, trailRadius) }
+    this.setStroke(ROBOT_COLOR)
+    trail.forEach { this.fillCircle(it.x, it.y, TRAIL_RADIUS) }
     return this
 }
