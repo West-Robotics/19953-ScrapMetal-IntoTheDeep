7422c2ca6eccca5b98a493414162e2b4ff0cd547
riddle-wrapped <mariakingry@proton.me>
2024-12-08 14:28:34 -0900

    feat: fix edge detectors in SubsystemTests and Teleop

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/ninth/opmode/SubsystemTests.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/ninth/opmode/SubsystemTests.kt
@@ -1,30 +1,38 @@
 package org.firstinspires.ftc.teamcode.ninth.opmode
 
+import com.acmerobotics.dashboard.config.Config
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.Gamepad
-import org.firstinspires.ftc.teamcode.ninth.controlEffort
 import org.firstinspires.ftc.teamcode.ninth.robot.subsystem.Drivetrain
-import org.firstinspires.ftc.teamcode.ninth.robot.subsystem.Lift
 import org.firstinspires.ftc.teamcode.ninth.robot.subsystem.Sampler
 
 // TODO: migrate relevant teleop logic out of subsystem tests
 
 @TeleOp(name = "SubsystemTests")
+//@Config
 class SubsystemTests : LinearOpMode() {
+//    @JvmField var stow = 0.0
+//    @JvmField var score = 0.0
+
     override fun runOpMode() {
         val previousGamepad1 = Gamepad()
+        val currentGamepad1 = Gamepad()
 
         val drivetrain = Drivetrain(hardwareMap)
         var desiredPos = 0.0
 //        val lift = Lift(hardwareMap)
         var manual = false
-        val extensionAmount = 0.0
+//        var extensionAmount = 0.43
         val sampler = Sampler(hardwareMap)
+        var intakeToggle = false
+        var extensionToggle = false
+//        var score = 0.43
 
         waitForStart()
         while (opModeIsActive()) {
-            previousGamepad1.copy(gamepad1)
+            previousGamepad1.copy(currentGamepad1)
+            currentGamepad1.copy(gamepad1)
 
 //            if (gamepad1.a && previousGamepad1.a) {
 //                desiredPos = 0.0
@@ -60,22 +68,40 @@ class SubsystemTests : LinearOpMode() {
 //            lift.write()
 
             // sampler
-//            sampler.setExtensionAmount(extensionAmount)
 
-//            if (gamepad1.a && !previousGamepad1.a) {
-//                sampler.extend()
-//            }
-//            if (gamepad1.a && previousGamepad1.a) {
-//                sampler.retract()
-//            }
-//            if (gamepad1.b && !previousGamepad1.b) {
-//                sampler.grab()
-//            }
-//            if (gamepad1.b && previousGamepad1.b) {
-//                sampler.stow()
-//            }
-//            if (gamepad1.x) {
+            if (currentGamepad1.a && !previousGamepad1.a) {
+                if (extensionToggle) {
+                    sampler.extend()
+                } else {
+                    sampler.retract()
+                }
+                extensionToggle = !extensionToggle
+            }
+
+            if (currentGamepad1.b && !previousGamepad1.b) {
+                if (intakeToggle) {
+                    sampler.grab()
+                } else {
+                    sampler.stow()
+                }
+                intakeToggle = !intakeToggle
+            }
+//
+//            if (currentGamepad1.x && !previousGamepad1.x) {
 //                sampler.score()
+//            }
+//            if (currentGamepad1.y && !previousGamepad1.y) {
+//                extensionAmount = 0.0
+//            }
+//            if (currentGamepad1.y && previousGamepad1.y) {
+//                extensionAmount = 0.68
+//            }
+
+//            if (gamepad1.dpad_up && !previousGamepad1.dpad_up) {
+//                score += 0.01
+//            }
+//            if (gamepad1.dpad_down && !previousGamepad1.dpad_down) {
+//                score -= 0.01
 //            }
 
             telemetry.addLine("Raise lift - hold left trigger")
@@ -90,6 +116,10 @@ class SubsystemTests : LinearOpMode() {
 //            telemetry.addData("Lift height", liftHeight)
 //            telemetry.addData("Control Effort", lift.getEffort())
 //            telemetry.addData("Desired position", desiredPos)
+//            telemetry.addData("Scoring pos", score)
+//            telemetry.addData("Scoring pos", score)
+//            telemetry.addData("extension amount", extensionAmount)
+
             telemetry.update()
         }
     }
