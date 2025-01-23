c57da1d1a2cfd57e744788739a17b03b33d26080
riddle_wrapped <149189755+a-riddle-wrapped-in-mysteryy@users.noreply.github.com>
2025-01-22 18:45:59 -0900

    feat: complete analog wrapper class

--- Util/src/main/kotlin/com/scrapmetal/util/hardware/SMAnalog.kt
+++ Util/src/main/kotlin/com/scrapmetal/util/hardware/SMAnalog.kt
@@ -1,4 +1,23 @@
 package com.scrapmetal.util.hardware
 
-class SMAnalog {
+import com.qualcomm.robotcore.hardware.AnalogInput
+import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.util.ElapsedTime
+
+class SMAnalog (
+    hardwareMap: HardwareMap,
+    name: String,
+) {
+    private val analogInput = hardwareMap.analogInput.get(name)
+    private var lastPosition = analogInput.voltage / 3.3 * 360
+    private val readTimer = ElapsedTime()
+
+    val velocity
+        get(): Double {
+            readTimer.reset()
+            val currentPosition = analogInput.voltage / 3.3 * 360
+            val velocity = (currentPosition - lastPosition) / readTimer.seconds()
+            lastPosition = currentPosition
+            return velocity
+        }
 }
